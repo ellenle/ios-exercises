dd90a829214598b561fbabf097e1bae14930f516
Ellen <ellenleis@gmail.com>
2015-11-16 23:44:56 -0800

    checkpoint 15 completed, simple data

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,55 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    int numberOne = [number intValue];
+    int doubledNumber = numberOne*2;
+    return [NSNumber numberWithInt:doubledNumber];
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    NSInteger arrayLength;
+    NSInteger smallNumber = 0;
+    NSInteger bigNumber;
+    if (number > otherNumber) {
+        arrayLength = number - otherNumber;
+        smallNumber = otherNumber;
+        bigNumber = number;
+    } else if (otherNumber > number) {
+        arrayLength = otherNumber - number;
+        smallNumber = number;
+        bigNumber = otherNumber;
+    }
+
+    NSMutableArray *finalArray = [[NSMutableArray alloc] init];
+    
+    for( int i = 0; i <= arrayLength; i++ )
+    {
+        [finalArray insertObject:[NSNumber numberWithLong:smallNumber] atIndex:i];
+        smallNumber = smallNumber +1;
+        NSLog(@"The content of arry is%@",finalArray);
+    }
+    return finalArray;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+    NSMutableArray *mutableArrayOfNumbers = [arrayOfNumbers mutableCopy];
+    [mutableArrayOfNumbers sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        NSNumber *numberA = (NSNumber *)obj1;
+        NSNumber *numberB = (NSNumber *)obj2;
+        
+        int intValueA = [numberA intValue];
+        int intValueB = [numberB intValue];
+        
+        if (intValueA > intValueB) {
+            return NSOrderedDescending;
+        } else if (intValueA < intValueB) {
+            return NSOrderedAscending;
+        }
+        return NSOrderedSame;
+    }];
+    NSNumber *returnValue = [mutableArrayOfNumbers objectAtIndex:0];
+    NSInteger returnValueInt = [returnValue integerValue];
+    return returnValueInt;
 }
 
 @end
